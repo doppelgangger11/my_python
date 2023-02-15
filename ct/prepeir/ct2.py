a05164ef108d02340eabe71333e67a01827c0e52
doppelgangger11 <113543404+doppelgangger11@users.noreply.github.com>
2023-02-15 10:30:36 +0600

    Delete ct directory

--- ct/prepeir/ct2.py
+++
@@ -1,8 +0,0 @@
-n = input().split()
-cc = 0
-for i in range(len(n)):
-    c = n[i]
-    if c[0] == 't' and c[-1] == 't':
-        cc += 1
-
-print(cc)
--- ct/prepeir/ct4.py
+++
@@ -1,7 +0,0 @@
-n = input()
-c = ['a', 'b', 'c']
-cc = str()
-for i in range(len(n)):
-    if n[i] != 'a' and n[i] != 'b' and n[i] != 'c':
-        cc += n[i]
-print(cc)
